3f956d852e4cde1d2619f5e2fa6ea1820dab2a6e
wenjieyek <dx3152@hotmail.com>
2017-06-01 20:23:19 +0800

    footer added

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
 
 
 
-  resources :school_users,:except=>[:show] do
+  resources :school_users do
+
+
+      
+  end
 
   	
 
 
 
-  end
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
