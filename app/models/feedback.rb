d50f3640b602e06cbdf30813f58228e8d42801fa
wenjieyek <dx3152@hotmail.com>
2017-06-15 13:15:49 +0800

    fix feedback email validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,9 @@
 class Feedback < ApplicationRecord
 
+	EMAIL_REGEX=/\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
 	validates :name,:presence=>true
-	validates :email,:presence=>true
+	validates :email,:presence=>true,:format=>EMAIL_REGEX
 	validates :phonenumber,:presence=>true
 	validates :title,:presence=>true
 	validates :details,:presence=>true
