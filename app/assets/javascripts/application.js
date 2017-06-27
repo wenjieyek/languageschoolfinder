d3a1951ffc6d5b8ef3197697902663015a531054
wenjieyek <dx3152@hotmail.com>
2017-06-27 17:39:18 +0800

    add sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,16 @@ document.addEventListener("turbolinks:load", function() {
 
     $('.collapsible').collapsible();
 
+    $('.button-collapse').sideNav({
+				      menuWidth: 300, // Default is 300
+				      edge: 'right', // Choose the horizontal origin
+				      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+				      draggable: true, // Choose whether you can drag to open on touch screens,
+				      onOpen: function(el) {  }, // A function to be called when sideNav is opened
+				      onClose: function(el) {  }, // A function to be called when sideNav is closed
+    }
+  );
+
   });
 });
 
