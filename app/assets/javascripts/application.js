4fa7c5bdaf4b638eecf5056884a906d508d731d4
wenjieyek <dx3152@hotmail.com>
2017-10-09 22:25:35 +0800

    test google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,6 +56,13 @@ document.addEventListener("turbolinks:load", function() {
       $('.materialboxed').materialbox();
 
 
+
+
+        if typeof ga is 'function'
+    ga('set', 'location', event.data.url)
+    ga('send', 'pageview')
+
+
     
 
   });
